5ec121ce8e946a7975c80b13fc1d60f2371637f8
Chris0xdeadbeef <christopher.ristic@yahoo.fr>
2025-12-04 15:52:06 +0100

    feat(todo.model): conversion en mongoose

--- backend/models/todo.model.js
+++ backend/models/todo.model.js
@@ -1,39 +1,30 @@
-module.exports = (sequelize, DataTypes) => {
-  const isSqlite = sequelize.getDialect() === 'sqlite';
+const mongoose = require('mongoose');
 
-  const Todo = sequelize.define(
-    'todo',
-    {
-      text: {
-        // In SQLite, TEXT is already 'long' by default (for the tests)
-        type: isSqlite ? DataTypes.TEXT : DataTypes.TEXT('long'),
-        allowNull: false
-      },
-      date: {
-        type: DataTypes.DATEONLY,
-        allowNull: true
-      },
-      completed: {
-        type: DataTypes.BOOLEAN,
-        allowNull: false
-      },
-      user_id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        references: {
-          model: 'users',
-          key: 'id'
-        },
-        onDelete: 'CASCADE'
-      }
+const TodoSchema = new mongoose.Schema(
+  {
+    text: {
+      type: String,
+      required: true
     },
-    {
-      indexes: [
-        // FULLTEXT only exists in MySQL, so disable on SQLite (for the tests)
-        ...(isSqlite ? [] : [{ type: 'FULLTEXT', name: 'text_idx', fields: ['text'] }])
-      ]
+    date: {
+      type: Date,
+      required: false
+    },
+    completed: {
+      type: Boolean,
+      required: true
+    },
+    user_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true
     }
-  );
+  },  
+  {
+    indexes: []
+  }
+);
+//index text en MongoDB, équivalent FULLTEXT (MySQL)
+TodoSchema.index({text: "text"})
 
-  return Todo;
-};
+module.exports = mongoose.model("Todo", TodoSchema)
