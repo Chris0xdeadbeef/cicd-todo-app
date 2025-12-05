6e66a4c68f8ac7c9eb8e98abba1aa5f4628c34cc
Chris0xdeadbeef <christopher.ristic@yahoo.fr>
2025-12-05 08:50:22 +0100

    fix(repo): mongo migration

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,13 +1,5 @@
-const { DataTypes } = require('sequelize');
+const User = require('./user.model');
+const Todo = require('./todo.model');
 
-const UserModel = require('./user.model');
-const TodoModel = require('./todo.model');
+module.exports = { User, Todo };
 
-function initModels(sequelize) {
-  const User = UserModel(sequelize, DataTypes);
-  const Todo = TodoModel(sequelize, DataTypes);
-
-  return { User, Todo };
-}
-
-module.exports = { initModels };
